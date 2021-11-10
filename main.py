e17c3d07b2e2a9f1f02ca7368d7b50b11533e98e
tamar shnirer <tamarsh224@gmail.com>
2021-11-10 19:29:53 +0200

    data exploration

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pandas
 import sklearn
 import numpy
+
+def gender_encoder(gender):
+    if gender=='M':
+        return 0
+    if gender=='F':
+        return 1
+    
+def survival_encoder(survival):
+    if survival=="Yes":
+        return 1
+    if survival=="No":
+        return 0
+    
