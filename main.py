7baae544bd40924b608d53048f9b510b36ced4fe
LinBurg0108 <93861782+LinBurg0108@users.noreply.github.com>
2021-11-20 17:24:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,4 +13,38 @@ def survival_encoder(survival):
         return 1
     if survival=="No":
         return 0
+
+def binary_extranodal_sites(number):
+    if number >= 1:
+        return 1
+    if number == 0:
+        return 0
     
+def get_mean_ipi(number):
+    if number>5:
+        digits = [int(d) for d in str(number)]
+        return sum(digits)/len(digits)
+    else:
+        return number
+    
+def get_min_ipi(number):
+    if number>5:
+        digits = [int(d) for d in str(number)]
+        return min(number)
+    else:
+        return number
+    
+def get_max_ipi(number):
+    if number>5:
+        digits = [int(d) for d in str(number)]
+        return max(number)
+    else:
+        return number
+    
+def impute_ipi_group(number):
+    if 0<=number<2:
+        return 'Low'
+    elif 2<=number<4:
+        return 'Intermediate'
+    else:
+        return 'High'
